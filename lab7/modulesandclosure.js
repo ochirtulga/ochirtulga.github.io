bc75a0540ae28b4b01edf5216754188f38496346
Ochirtulga <ochiroocho@gmail.com>
2022-01-18 15:35:56 -0600

    lab7

--- lab7/modulesandclosure.js
+++ lab7/modulesandclosure.js
@@ -1,15 +1,11 @@
 "use strict";
 
-window.onload = () => { 
-    
+window.onload = () => {
+
     var createBtn = document.getElementById("createNew");
 
-    var createAccount = function () { 
-        var account;
-        var accountName = document.getElementById("accName");
-        var balance = document.getElementById("deposit");
-        account = new Account(accountName.value, balance.value);
-        return account;
+    var accountFactory = (accountName, balance) => {
+        return new Account(accountName, balance);;
     }
 
     class Account {
@@ -19,21 +15,23 @@ window.onload = () => {
         }
     }
 
-    createBtn.onclick = () => { 
-        var account = (createAccount());
-        console.log(account);
+    createBtn.onclick = () => {
+
+        const accountName = document.getElementById("accName");
+        const balance = document.getElementById("deposit");
+        var account = accountFactory(accountName.value, balance.value);
+
         updateTextArea(account);
     }
 
     function updateTextArea(account) {
-        if(document.getElementById("textArea").value.length == 0) {
-            document.getElementById("textArea").value = document.getElementById("textArea").value 
-            + "Account Name:  " + account.accountName + " Balance:  " + account.balance;
+        if (document.getElementById("textArea").value.length == 0) {
+            document.getElementById("textArea").value = document.getElementById("textArea").value +
+                "Account Name:  " + account.accountName + " Balance:  " + account.balance;
         } else {
-            document.getElementById("textArea").value = document.getElementById("textArea").value 
-            + "\n" 
-            + "Account Name:  " + account.accountName + " Balance:  " + account.balance;
+            document.getElementById("textArea").value = document.getElementById("textArea").value +
+                "\n" +
+                "Account Name:  " + account.accountName + " Balance:  " + account.balance;
         }
     }
 }
-
