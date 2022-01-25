5266033cc08a6f0dd4c541518e6661167596e3ee
Ochirtulga <ochiroocho@gmail.com>
2022-01-24 21:33:13 -0600

    hw9

---
+++ hw9/fifteen.js
@@ -0,0 +1,96 @@
+$(document).ready(function () {
+    // var puzzleArea = $("puzzlearea");
+    // var divs = $("#puzzlearea > div");
+    var empty = (function () {
+        let x = 300;
+        let y = 300;
+        let changePosition = function (a, b) {
+            x = a;
+            y = b;
+        };
+        let getX = () => {
+            return x;
+        }
+        let getY = () => {
+            return y;
+        }
+        return {
+            changePosition: changePosition,
+            getX: getX,
+            getY: getY
+        }
+    })();
+
+    const divs = "#puzzlearea > div";
+
+    $(divs).each(function (idx, e) {
+        var x = ((idx % 4) * 100);
+        var y = (Math.floor(idx / 4) * 100);
+        $(e).addClass("puzzlepiece")
+            .css("left", x + 'px')
+            .css("top", y + 'px')
+            .css("backgroundImage", "url('../images/hw9/background.jpeg')")
+            .css("backgroundPosition", -x + 'px ' + (-y) + 'px');
+    });
+
+    $(divs).hover(
+        function () {
+            if (checkMovable(this) == true) {
+                $(this).addClass("movablepiece");
+            } else {
+                return false;
+            }
+        },
+        function () {
+            $(this).removeClass("movablepiece");
+        }
+    );
+
+    $(divs).click(function () {
+        if (checkMovable(this) == true) {
+            let currX = parseInt($(this).css("left"));
+            let currY = parseInt($(this).css("top"));
+            $(this).css("left", empty.getX() + 'px ')
+                .css("top", empty.getY() + 'px');
+            empty.changePosition(currX, currY);
+        } else {
+            return false;
+        }
+    });
+
+    function checkMovable(piece) {
+        let currX = parseInt($(piece).css("left"));
+        let currY = parseInt($(piece).css("top"));
+        if (Math.abs(Math.abs(empty.getX() - currX) + Math.abs(empty.getY()) - currY) == 100) {
+            return true;
+        } else {
+            return false
+        };
+    }
+    // randomShuffle();
+    function randomShuffle() {
+        for(let i = 1; i < 16; i++) {
+            console.log(parseInt(Math.random() * 16)); 
+        }
+    }
+    // initialize each piece
+    // for (var i=0; i< divs.length; i++) {
+    //     var div = divs[i];
+
+    //     // calculate x and y for this piece
+    //     var x = ((i % 4) * 100) ;
+    //     var y = (Math.floor(i / 4) * 100) ;
+
+    //     // set basic style and background
+    //     div.className = "puzzlepiece";
+    //     div.style.left = x + 'px';
+    //     div.style.top = y + 'px';
+    //     div.style.backgroundImage = 'url("../images/hw9/background.jpeg")';
+    //     div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
+
+    //     // store x and y for later
+    //     div.x = x;
+    //     div.y = y; 
+    // }  
+
+});
