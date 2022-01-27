bf0c4320ffbfd77721f7b8f1094357701264f415
Ochirtulga <ochiroocho@gmail.com>
2022-01-27 12:02:11 -0600

    nodejs

---
+++ nodefolder/exerciseN1.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var dt = require('./myModule');
+
+http.createServer(function (req, res) {
+    res.writeHead(200,
+        {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDate() + "\n");
+    
+    res.end('Hello World!');
+    
+}).listen(8080);
